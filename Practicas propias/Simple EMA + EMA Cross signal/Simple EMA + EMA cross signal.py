9344b28da9e37cbbf01068a17b905081e8d5a768
Pippex <70782473+Pippex@users.noreply.github.com>
2021-08-29 20:15:42 -0500

    Anado una senal por EMA, intento hacer una por MACD, aunque no queda bien

--- Practicas propias/Simple EMA + EMA Cross signal/Simple EMA + EMA cross signal.py
+++ Practicas propias/Simple EMA + EMA Cross signal/Simple EMA + EMA cross signal.py
@@ -12,7 +12,7 @@ def get_MA(info, periods):
     return info.rolling(periods).mean()
 
 
-def get_EMA(info,periods):
+def get_EMA(info, periods):
     return info.ewm(span=periods).mean()
 
 
@@ -20,25 +20,47 @@ def empty_column(info):
     return info[list(info)[3]] - info[list(info)[3]]
 
 
-AMZN = get_chart("AMZN",datetime.datetime(2015, 1, 1),datetime.datetime(2020, 8, 28))
+def substract_dataframe(dataframe1, dataframe2):
+    return dataframe1 - dataframe2
 
-AMZN["EMA20"] = get_EMA(AMZN, 20)
-AMZN["EMA30"] = get_EMA(AMZN, 30)
-AMZN["SIGNAL"] = empty_column(AMZN)
 
-for d in range(len(AMZN["EMA20"])):
-    if d+1 < len(AMZN["EMA20"]):
+def divide_dataframe(dataframe1, dataframe2):
+    return (dataframe1/dataframe2) ** 150
 
-        if AMZN["EMA20"][d] < AMZN["EMA30"][d]:
-            if AMZN["EMA20"][d+1] > AMZN["EMA30"][d+1]:
-                AMZN["SIGNAL"][d+1] = 1
 
-        if AMZN["EMA20"][d] > AMZN["EMA30"][d]:
-            if AMZN["EMA20"][d + 1] < AMZN["EMA30"][d + 1]:
-                AMZN["SIGNAL"][d + 1] = -1
+def main():
+    AMZN = get_chart("AMZN", datetime.datetime(2015, 1, 1), datetime.datetime(2020, 8, 28))
 
-print(AMZN["SIGNAL"])
+    AMZN["EMA20"] = get_EMA(AMZN["Close"], 20)
+    AMZN["EMA30"] = get_EMA(AMZN["Close"], 30)
+    AMZN["EMASIGNAL"] = empty_column(AMZN)
+    AMZN["MACDSIGNAL"] = empty_column(AMZN)
+    AMZN["MACD"] = divide_dataframe(AMZN["EMA20"], AMZN["EMA30"])
 
-plt.plot(AMZN["Close"], label="AMZN")
-plt.plot(AMZN["SIGNAL"], label = "Signal")
-plt.show()
+    for d in range(len(AMZN["EMA20"])):
+        if d+1 < len(AMZN["EMA20"]):
+
+            if AMZN["EMA20"][d] < AMZN["EMA30"][d]:
+                if AMZN["EMA20"][d+1] > AMZN["EMA30"][d+1]:
+                    AMZN["EMASIGNAL"][d+1] = AMZN["EMA30"][d]/10
+
+            if AMZN["EMA20"][d] > AMZN["EMA30"][d]:
+                if AMZN["EMA20"][d + 1] < AMZN["EMA30"][d + 1]:
+                    AMZN["EMASIGNAL"][d + 1] = AMZN["EMA30"][d]/(-10)
+
+    for d in range(len(AMZN["MACD"])):
+        if AMZN["MACD"][d] ** (1/150) >= 1.05:
+            AMZN["MACDSIGNAL"] = AMZN["Close"]/10
+
+    plt.plot(AMZN["Close"], label = "AMZN")
+    plt.plot(AMZN["EMA20"], label = "EMA 20")
+    plt.plot(AMZN["EMA30"], label = "EMA 30")
+    plt.plot(AMZN["EMASIGNAL"], label = "EMA SIGNAL")
+    plt.plot(AMZN["MACD"], label = "MACD")
+    plt.plot(AMZN["MACDSIGNAL"], label = "MACD SIGNAL")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
